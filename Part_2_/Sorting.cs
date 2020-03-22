b3c2c03492247a36690367b9bf6408b28a5f8d23
Sherba Kirill <sherba.kirill@gmail.com>
2020-03-22 14:46:07 +0300

    Deleted TODO and NOTE

--- Part_2_/Sorting.cs
+++ Part_2_/Sorting.cs
@@ -4,50 +4,23 @@ namespace Part_2
 {
     public static class Sorting
     {
-        /// <summary>
-        /// Order.
-        /// Example: [3, 1, 4] => [1, 3, 4] 
-        /// </summary>
-        /// <typeparam name="T">Data type</typeparam>
-        /// <param name="collection">Collection to sort</param>
-        /// <returns>Ordered collection</returns>
         public static T[] Order<T>(T[] collection)
             where T : IComparable<T>, IComparable
         {
-            // NOTE: do not use LINQ
-            // TODO: implement here
             Array.Sort(collection);
             return collection;
         }
 
-        /// <summary>
-        /// Order by descending.
-        /// Example: [3, 1, 4] => [4, 3, 1] 
-        /// </summary>
-        /// <typeparam name="T">Data type</typeparam>
-        /// <param name="collection">Collection to sort</param>
-        /// <returns>Ordered collection</returns>
         public static T[] DescendingOrder<T>(T[] collection)
             where T : IComparable<T>, IComparable
         {
-            // NOTE: do not use LINQ
-            // TODO: implement here
             Array.Sort(collection);
             Array.Reverse(collection);
             return collection;
         }
 
-        /// <summary>
-        /// Find unique collection elements.
-        /// Example: [3, 1, 1, 4, 3, 3, 1, 4] => [3, 1, 4] 
-        /// </summary>
-        /// <typeparam name="T">Data type</typeparam>
-        /// <param name="collection">Collection to sort</param>
-        /// <returns>Ordered collection</returns>
         public static T[] Unique<T>(T[] collection)
         {
-            // NOTE: do not use LINQ
-            // TODO: implement here
             T[] result = new T[0];
             foreach (T element in collection)
                 if (Array.IndexOf(result, element) == -1)
